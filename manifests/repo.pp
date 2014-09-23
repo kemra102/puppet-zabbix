1a2d97a6f1a0d8cc3009ed292901bf60366edfc4
Danny Roberts <danny.roberts@reconnix.com>
2014-09-23 14:46:37 +0100

    Fixed typo

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -3,7 +3,7 @@ class zabbix::repo inherits zabbix::params {
   yumrepo { 'zabbix':
     ensure	=> present,
     descr	=> 'Zabbix Official Repository - $basearch',
-    baseurl	=> "http://repo.zabbix.com/zabbix/$::zabbix::versio/rhel/$::operatingsystemmajrelease/$::architecture/",
+    baseurl	=> "http://repo.zabbix.com/zabbix/$version/rhel/$::operatingsystemmajrelease/$::architecture/",
     enabled	=> true,
     gpgkey	=> 'http://repo.zabbix.com/RPM-GPG-KEY-ZABBIX',
     gpgcheck	=> true,
