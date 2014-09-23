857b0d2670c22b77ff53ef5b2ce8d004ff241d93
Danny Roberts <danny.roberts@reconnix.com>
2014-09-23 16:44:11 +0100

    Added a dependcy for the service on the config file to properly order the execution

--- manifests/client/service.pp
+++ manifests/client/service.pp
@@ -5,6 +5,7 @@ class zabbix::client::service inherits zabbix::params {
     enable	=> $client_service_enable,
     hasstatus	=> true,
     hasrestart	=> true,
+    require	=> File['/etc/zabbix/zabbix_agentd.conf'],
   }
 
 }
