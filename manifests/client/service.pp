6c7f42cf688596fc05da1f03ba445533660eafcc
Danny Roberts <danny.roberts@reconnix.com>
2014-09-24 10:26:39 +0100

    Added required checks so that the minimum configuration options are entered for zabbix client

--- manifests/client/service.pp
+++ manifests/client/service.pp
@@ -1,11 +1,19 @@
 class zabbix::client::service inherits zabbix::params {
 
-  service { 'zabbix-agent':
-    ensure	=> $client_service_ensure,
-    enable	=> $client_service_enable,
-    hasstatus	=> true,
-    hasrestart	=> true,
-    require	=> File['/etc/zabbix/zabbix_agentd.conf'],
+  if ($client_startagents != '0' and $client_server == '') {
+    fail("Your StartAgents cannot be greater than 0 or null when Server is also null. Set StartAgents to 0 or set Server.")
+  }
+  if ($client_server == '' and $client_serveractive == '') {
+    fail("You must set either passvie or active (or both) checks via the Server or ServerActive options.")
+  }
+  else {
+    service { 'zabbix-agent':
+      ensure            => $client_service_ensure,
+      enable            => $client_service_enable,
+      hasstatus         => true,
+      hasrestart        => true,
+      require           => File['/etc/zabbix/zabbix_agentd.conf'],
+    }
   }
 
 }
