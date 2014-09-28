f454fe2b209f7d9c403be24b833edba5e1610729
Danny Roberts <danny@thefallenphoenix.net>
2014-09-28 14:54:05 +0100

    Fixed more scoping issues

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,4 +1,64 @@
-class zabbix::client {
+class zabbix::client (
+
+  # Allow for overrides of variables in params.pp
+  $service_ensure       = $zabbix::params::client_service_ensure,
+  $service_enable       = $zabbix::params::client_service_enable,
+
+  $pidfile              = $zabbix::params::client_pidfile,
+  $logfile              = $zabbix::params::client_logfile,
+  $logfilesize          = $zabbix::params::client_logfilesize,
+  $debuglevel           = $zabbix::params::client_debuglevel,
+  $sourceip             = $zabbix::params::client_sourceip,
+  $enableremotecommands = $zabbix::params::client_enableremotecommands,
+  $logremotecommands    = $zabbix::params::client_logremotecommands,
+  $server               = $zabbix::params::client_server,
+  $listenport           = $zabbix::params::client_listenport,
+  $listenip             = $zabbix::params::client_listenip,
+  $startagents          = $zabbix::params::client_startagents,
+  $serveractive         = $zabbix::params::client_serveractive,
+  $hostname             = $zabbix::params::client_hostname,
+  $hostnameitem         = $zabbix::params::client_hostnameitem,
+  $refreshactivechecks  = $zabbix::params::client_refreshactivechecks,
+  $buffersend           = $zabbix::params::client_buffersend,
+  $buffersize           = $zabbix::params::client_buffersize,
+  $maxlinespersecond    = $zabbix::params::client_maxlinespersecond,
+  $allowroot            = $zabbix::params::client_allowroot,
+  $parameteralias       = $zabbix::params::client_parameteralias,
+  $timeout              = $zabbix::params::client_timeout,
+  $include              = $zabbix::params::client_include,
+  $unsafeuserparameters = $zabbix::params::client_unsafeuserparameters,
+  $userparameter        = $zabbix::params::client_userparameter,
+
+  ) inherits zabbix::params {
+
+  # Validate our variables
+  validate_string($client::service_ensure)
+  validate_bool($client::service_enable)
+
+  validate_absolute_path($client::pidfile)
+  validate_absolute_path($client::logfile)
+  validate_string($client::logfilesize)
+  validate_string($client::debuglevel)
+  validate_string($client::sourecip)
+  validate_string($client::enableremotecommands)
+  validate_string($client::logremotecommands)
+  validate_string($client::server)
+  validate_string($client::listenport)
+  validate_string($client::listenip)
+  validate_string($client::startagents)
+  validate_string($client::serveractive)
+  validate_string($client::hostname)
+  validate_string($client::hostnameitem)
+  validate_string($client::refreshactivechecks)
+  validate_string($client::buffersend)
+  validate_string($client::buffersize)
+  validate_string($client::maxlinespersecond)
+  validate_string($client::allowroot)
+  validate_string($client::alias)
+  validate_string($client::timeout)
+  validate_absolute_path($client::include)
+  validate_string($client::unsafeuserparameters)
+  validate_string($client::userparameter)
 
   include ::zabbix::client::install
   include ::zabbix::client::service
--- manifests/init.pp
+++ manifests/init.pp
@@ -37,64 +37,6 @@
 #
 class zabbix (
 
-  # Allow for overrides of variables in params.pp
-  $client::service_ensure	= $zabbix::params::client::service_ensure,
-  $client::service_enable	= $zabbix::params::client::service_enable,
-
-  $client::pidfile		= $zabbix::params::client::pidfile,
-  $client::logfile		= $zabbix::params::client::logfile,
-  $client::logfilesize		= $zabbix::params::client::logfilesize,
-  $client::debuglevel		= $zabbix::params::client::debuglevel,
-  $client::sourceip		= $zabbix::params::client::sourceip,
-  $client::enableremotecommands	= $zabbix::params::client::enableremotecommands,
-  $client::logremotecommands	= $zabbix::params::client::logremotecommands,
-  $client::server		= $zabbix::params::client::server,
-  $client::listenport		= $zabbix::params::client::listenport,
-  $client::listenip		= $zabbix::params::client::listenip,
-  $client::startagents		= $zabbix::params::client::startagents,
-  $client::serveractive		= $zabbix::params::client::serveractive,
-  $client::hostname		= $zabbix::params::client::hostname,
-  $client::hostnameitem		= $zabbix::params::client::hostnameitem,
-  $client::refreshactivechecks	= $zabbix::params::client::refreshactivechecks,
-  $client::buffersend		= $zabbix::params::client::buffersend,
-  $client::buffersize		= $zabbix::params::client::buffersize,
-  $client::maxlinespersecond	= $zabbix::params::client::maxlinespersecond,
-  $client::allowroot		= $zabbix::params::client::allowroot,
-  $client::alias		= $zabbix::params::client::alias,
-  $client::timeout		= $zabbix::params::client::timeout,
-  $client::include		= $zabbix::params::client::include,
-  $client::unsafeuserparameters	= $zabbix::params::client::unsafeuserparameters,
-  $client::userparameter	= $zabbix::params::client::userparameter,
-
-) inherits zabbix::params {
-
-  # Validate our variables
-  validate_string($client::service_ensure)
-  validate_bool($client::service_enable)
-
-  validate_absolute_path($client::pidfile)
-  validate_absolute_path($client::logfile)
-  validate_string($client::logfilesize)
-  validate_debuglevel($client::debuglevel)
-  validate_string($client::sourecip)
-  validate_string($client::enableremotecommands)
-  validate_string($client::logremotecommands)
-  validate_string($client::server)
-  validate_string($client::listenport)
-  validate_string($client::listenip)
-  validate_string($client::startagents)
-  validate_string($client::serveractive)
-  validate_string($client::hostname)
-  validate_string($client::hostnameitem)
-  validate_string($client::refreshactivechecks)
-  validate_string($client::buffersend)
-  validate_string($client::buffersize)
-  validate_string($client::maxlinespersecond)
-  validate_string($client::allowroot)
-  validate_string($client::alias)
-  validate_string($client::timeout)
-  validate_absolute_path($client::include)
-  validate_string($client::unsafeuserparameters)
-  validate_string($client::userparameter)
+  include ::zabbix::client
 
 }
