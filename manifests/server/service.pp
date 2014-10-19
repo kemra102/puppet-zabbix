e9aad368679004a493563bc01c9d5cd2e30a04a3
Danny Roberts <danny@thefallenphoenix.net>
2014-10-19 23:20:48 +0100

    Initial code for zabbix server support

---
+++ manifests/server/service.pp
@@ -0,0 +1,20 @@
+class zabbix::server::service (
+
+  $service_manage       = $zabbix::server::service_manage,
+  $service_ensure       = $zabbix::server::service_ensure,
+  $service_enable       = $zabbix::server::service_enable,
+
+) inherits zabbix::server {
+
+  if $service_manage == true {
+    service { 'zabbix-server':
+      ensure            => $service_ensure,
+      enable            => $service_enable,
+      hasstatus         => true,
+      hasrestart        => true,
+      require           => File['/etc/zabbix/zabbix_server.conf'],
+      subscribe         => File['/etc/zabbix/zabbix_server.conf'],
+    }
+  }
+
+}
