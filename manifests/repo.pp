7ce5b8daf21336a4dd947ed97b4a245614170f0b
Danny Roberts <danny.roberts@reconnix.com>
2015-01-13 17:25:12 +0000

    Properly scoped version variable so the version would correctly be propogated to yum/apt repos

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -1,11 +1,15 @@
-class zabbix::repo inherits zabbix::params {
+class zabbix::repo (
+
+  $version = $::zabbix::params::version
+
+) inherits zabbix::params {
 
   case $::osfamily {
     'RedHat': {
       yumrepo { 'zabbix':
         ensure   => present,
         descr    => 'Zabbix Official Repository - $basearch',
-        baseurl  => "http://repo.zabbix.com/zabbix/${zabbix::version}/rhel/${::operatingsystemmajrelease}/${::architecture}/",
+        baseurl  => "http://repo.zabbix.com/zabbix/${version}/rhel/${::operatingsystemmajrelease}/${::architecture}/",
         enabled  => true,
         gpgkey   => 'http://repo.zabbix.com/RPM-GPG-KEY-ZABBIX',
         gpgcheck => true,
@@ -23,7 +27,7 @@ class zabbix::repo inherits zabbix::params {
       case $::operatingsystem {
         'Ubuntu': {
           apt::source { 'ubuntu-zabbix':
-            location   => "http://repo.zabbix.com/zabbix/${zabbix::version}/ubuntu/",
+            location   => "http://repo.zabbix.com/zabbix/${version}/ubuntu/",
             release    => "${::lsbdistcodename}",
             repos      => 'main non-free contrib',
             key        => '79EA5ED4',
@@ -32,7 +36,7 @@ class zabbix::repo inherits zabbix::params {
         }
         'Debian': {
           apt::source { 'debian-zabbix':
-            location   => "http://repo.zabbix.com/zabbix/${zabbix::version}/debian/",
+            location   => "http://repo.zabbix.com/zabbix/${version}/debian/",
             release    => "${::lsbdistcodename}",
             repos      => 'main non-free contrib',
             key        => '79EA5ED4',
